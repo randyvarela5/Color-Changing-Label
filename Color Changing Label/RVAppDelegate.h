aebd84e11a0d214860ea3b66aecfbd275d5bbc96
Randy Varela <randyvarela@Randys-MacBook-Pro.local>
2014-02-16 12:12:16 -0500

    Initial Commit

---
+++ Color Changing Label/RVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RVAppDelegate.h
+//  Color Changing Label
+//
+//  Created by Randy Varela on 2/16/14.
+//  Copyright (c) 2014 Randy Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
